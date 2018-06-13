747ee2e4b31175c47373ffec5a9b4818dbc8bd08
MURAOKA Taro <koron.kaoriya@gmail.com>
2018-06-13 13:49:08 +0900

    initial file

---
+++ src/main/java/net/kaoriya/mapmap/Writer.java
@@ -0,0 +1,12 @@
+package net.kaoriya.mapmap;
+
+import java.io.IOException;
+import java.io.File;
+
+public class Writer {
+
+    private File baseFile;
+
+    public Writer(File file) throws IOException {
+    }
+}
